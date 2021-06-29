e12ce82a0d78493212e3897acc60e9a8cfbba08c
Jackson Kearl <jakearl@microsoft.com>
2021-06-29 00:52:27 -0700

    Compress

--- src/extension.ts
+++ src/extension.ts
@@ -11,7 +11,7 @@ export function activate(context: vscode.ExtensionContext) {
 						.trim()
 						.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')
 						.replace(/\W+/g, '\\W+'),
-				});
+				})
 		}
 	}))
 }
