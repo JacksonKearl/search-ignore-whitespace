26e194ecc91ebafaa40dfd68a6ab3f1fd8bcb8c1
Jackson Kearl <jakearl@microsoft.com>
2021-06-29 00:52:18 -0700

    Clean and compress

--- src/extension.ts
+++ src/extension.ts
@@ -1,21 +1,17 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-
-	let disposable = vscode.commands.registerTextEditorCommand('search-ignore-whitespace.searchIgnoreWhitespace', (editor) => {
-		const selection = editor.selection;
+	context.subscriptions.push(vscode.commands.registerTextEditorCommand('search-ignore-whitespace.searchIgnoreWhitespace', (editor) => {
+		const selection = editor.selection
 		if (selection) {
-			const rawText = editor.document.getText(new vscode.Range(selection.start, selection.end));
-			const regexTransformed = rawText
-			.trim()
-			.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')
-			.replace(/\W+/g, '\\W+');
-
-			vscode.commands.executeCommand('workbench.action.findInFiles', {query: regexTransformed, isRegex: true});
+			vscode.commands.executeCommand('workbench.action.findInFiles',
+				{
+					isRegex: true,
+					query: editor.document.getText(new vscode.Range(selection.start, selection.end))
+						.trim()
+						.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')
+						.replace(/\W+/g, '\\W+'),
+				});
 		}
-	});
-
-	context.subscriptions.push(disposable);
+	}))
 }
-
-export function deactivate() {}
